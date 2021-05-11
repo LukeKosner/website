ddd112d8a2684f8cefc4075f79a1666979116c3b
Luke Kosner <29708877+lukekosner@users.noreply.github.com>
2021-05-11 17:44:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.listen(port);
