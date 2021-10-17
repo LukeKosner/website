a10c7f917de97cf8efc2ca7950595a6173b2c1a0
LukeKosner <hey@lukekosner.com>
2021-10-17 11:33:56 -0400

    PWA

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,7 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  './', // Alias for index.html
-  'zoom.html',
-  'leaving.html'
+  './' // Alias for index.html
 ];
 
 // The install handler takes care of precaching the resources we always need.
